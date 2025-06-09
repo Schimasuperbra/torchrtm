238c53c7317e48a9befcce22bad9edf4566f9be0
Peng Sun <01sunpeng@163.com>
2025-06-09 13:47:35 +0200

    Release version 1.1.1

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 import pathlib
 
-# Get the long description from the README file
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='torchrtm',
-    version='1.1.0',
-    description='Torch-based Vegatation Radiative Transfer Model library (PROSPECT, SAIL, SMAC)',
+    version='1.1.1',
+    description='Torch-based Vegetation Radiative Transfer Model library (PROSPECT, SAIL, SMAC)',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Peng Sun, Marco D. Visser',
     author_email='sunpeng18@mails.ucas.ac.cn',
-    url='https://github.com/Schimasuperbra/torchrtm',  # Replace with your actual repo URL
+    url='https://github.com/Schimasuperbra/torchrtm',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         'torch>=2.0.0',
         'numpy>=1.18',
         'pandas>=1.1',
         'scipy>=1.5',
-        "matplotlib",
-        "tqdm"
+        'matplotlib',
+        'tqdm',
     ],
     python_requires='>=3.9',
     classifiers=[
@@ -35,23 +35,16 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-
     ],
     keywords='radiative transfer, PROSAIL, SMAC, torch, remote sensing, inversion',
     project_urls={
-        'Source': 'https://github.com/Schimasuperbra/torchrtm',  # Replace accordingly
+        'Source': 'https://github.com/Schimasuperbra/torchrtm',
         'Bug Tracker': 'https://github.com/Schimasuperbra/torchrtm/issues',
     },
-    include_package_data=True,
     package_data={
-        "torchrtm": [
-            "data/*.csv",
-            "data/sensor_information/*.pkl",
+        'torchrtm': [
+            'data/*.csv',
+            'data/sensor_information/*.pkl',
         ],
     },
-
-
-    
 )
-
-
